03aa03877ff360c2a6b6d5612f95555806ebc3a5
queryabduction <bohemianccc@gmail.com>
2022-05-06 14:39:52 +0800

    delete comments

--- src/main/java/org/gu/dcore/modularization/Modularizor.java
+++ src/main/java/org/gu/dcore/modularization/Modularizor.java
@@ -1,25 +1,5 @@
 package org.gu.dcore.modularization;
-/*
- * Copyright (C) 2018 - 2020 Artificial Intelligence and Semantic Technology, 
- * Griffith University
- * 
- * Contributors:
- * Peng Xiao (sharpen70@gmail.com)
- * Zhe wang
- * Kewen Wang
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 import java.util.LinkedList;
 import java.util.List;
 
@@ -31,56 +11,58 @@ public class Modularizor {
 	private BaseMarking marking;
 	private IndexedBlockRuleSet ibs;
 	private List<BlockRule> blockonto;
-	
+
 	public Modularizor(List<Rule> onto) {
 		this.onto = onto;
 		this.marking = new BaseMarking(onto);
 	}
-	
+
 	public void modularize() {
-//		System.out.println("Modularizing ontology ...");
+		// System.out.println("Modularizing ontology ...");
 		long start = System.currentTimeMillis();
-		
+
 		this.ibs = new IndexedBlockRuleSet();
 		this.blockonto = new LinkedList<>();
-		 
-		for(Rule r : onto) {
+
+		for (Rule r : onto) {
 			this.marking.mark(r);
 		}
 
-		for(Rule r: onto) {
+		for (Rule r : onto) {
 			BlockRule br = this.marking.getBlockRule(r);
 			this.ibs.add(br);
 			this.blockonto.add(br);
 		}
-//		System.out.println("Done modularization, taking " + (System.currentTimeMillis() - start) + " ms");
+		// System.out.println("Done modularization, taking " +
+		// (System.currentTimeMillis() - start) + " ms");
 	}
-	
+
 	public IndexedBlockRuleSet getIndexedBlockOnto() {
-		if(this.ibs == null) modularize();
+		if (this.ibs == null)
+			modularize();
 		return this.ibs;
 	}
-	
-	
+
 	public List<BlockRule> getBlockOnto() {
-		if(this.blockonto == null) modularize();
+		if (this.blockonto == null)
+			modularize();
 		return this.blockonto;
 	}
-//	public List<BlockRule> getBlockRules(Rule r, List<Block> blocks) {
-//		List<BlockRule> blockRules = new LinkedList<>();
-//		
-//		blockRules.add(new BlockRule(r));
-//		
-//		for(int i = 0; i < blockRules.size(); i++) {
-//			for(int j = i; j < blocks.size(); j++) {
-//				BlockRule nbr = blockRules.get(i).add(blocks.get(j));
-//				if(nbr != null) blockRules.add(nbr);
-//			}
-//		}
-//		
-//		return blockRules;
-//	}
-	
+	// public List<BlockRule> getBlockRules(Rule r, List<Block> blocks) {
+	// List<BlockRule> blockRules = new LinkedList<>();
+	//
+	// blockRules.add(new BlockRule(r));
+	//
+	// for(int i = 0; i < blockRules.size(); i++) {
+	// for(int j = i; j < blocks.size(); j++) {
+	// BlockRule nbr = blockRules.get(i).add(blocks.get(j));
+	// if(nbr != null) blockRules.add(nbr);
+	// }
+	// }
+	//
+	// return blockRules;
+	// }
+
 	public BaseMarking getMarking() {
 		return this.marking;
 	}
--- src/main/java/org/gu/dcore/parsing/DcoreOWL2Parser.java
+++ src/main/java/org/gu/dcore/parsing/DcoreOWL2Parser.java
@@ -1,25 +1,5 @@
 package org.gu.dcore.parsing;
-/*
- * Copyright (C) 2018 - 2020 Artificial Intelligence and Semantic Technology, 
- * Griffith University
- * 
- * Contributors:
- * Peng Xiao (sharpen70@gmail.com)
- * Zhe wang
- * Kewen Wang
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -61,25 +41,25 @@ import org.semanticweb.owlapi.model.OWLSubObjectPropertyOfAxiom;
 
 public class DcoreOWL2Parser {
 	private List<Rule> rules;
-	
+
 	int variable_counter = 0;
-	
+
 	public DcoreOWL2Parser() {
 	}
-	
+
 	public Program parseFile(String ontoFile) throws OWLOntologyCreationException {
 		rules = new LinkedList<>();
-		
+
 		OWLOntologyManager manager = OWLManager.createConcurrentOWLOntologyManager();
 		OWLOntology ontology = manager.loadOntologyFromOntologyDocument(new File(ontoFile));
 		Set<OWLOntology> ontologies = manager.getImportsClosure(ontology);
-		
-		for(OWLOntology o : ontologies)
+
+		for (OWLOntology o : ontologies)
 			o.axioms().filter(new DcoreOWL2Filter()).forEach(a -> a.accept(new tboxAxiomVistor()));
-		
+
 		return new Program(rules);
 	}
-	
+
 	/*
 	 * At this stage, we consider only SubClassOf and SubPropertyOf axioms
 	 */
@@ -87,27 +67,28 @@ public class DcoreOWL2Parser {
 		@Override
 		public void visit(OWLSubClassOfAxiom axiom) {
 			DGLPClassVisitor visitor = new DGLPClassVisitor(0);
-			
+
 			variable_counter = 0;
 			AtomSet body = axiom.getSubClass().accept(visitor);
 			AtomSet head = axiom.getSuperClass().accept(visitor);
-			
-			if(body != null && head != null)
+
+			if (body != null && head != null)
 				rules.add(RuleFactory.instance().createRule(head, body));
-			
+
 		};
+
 		@Override
 		public void visit(OWLEquivalentClassesAxiom axiom) {
 			OWLPairwiseVoidVisitor<OWLClassExpression> _vistor = new OWLPairwiseVoidVisitor<OWLClassExpression>() {
 				@Override
 				public void visit(OWLClassExpression a, OWLClassExpression b) {
 					DGLPClassVisitor visitor = new DGLPClassVisitor(0);
-					
+
 					variable_counter = 0;
 					AtomSet as = a.accept(visitor);
 					AtomSet bs = b.accept(visitor);
-					
-					if(as != null && bs != null) {
+
+					if (as != null && bs != null) {
 						rules.add(RuleFactory.instance().createRule(as, bs));
 						rules.add(RuleFactory.instance().createRule(bs, as));
 					}
@@ -115,159 +96,166 @@ public class DcoreOWL2Parser {
 			};
 			axiom.forEach(_vistor);
 		};
+
 		@Override
 		public void visit(OWLSubObjectPropertyOfAxiom axiom) {
 			OWLObjectPropertyExpression subp = axiom.getSubProperty();
 			OWLObjectPropertyExpression superp = axiom.getSuperProperty();
-			
+
 			DGLPPropertyVisitor visitor = new DGLPPropertyVisitor(0);
-			
+
 			variable_counter = 0;
 			AtomSet body = subp.accept(visitor);
 			variable_counter = 0;
 			AtomSet head = superp.accept(visitor);
-			
-			if(body != null && head != null)
+
+			if (body != null && head != null)
 				rules.add(RuleFactory.instance().createRule(head, body));
-		};		
+		};
+
 		@Override
-		public void visit(OWLObjectPropertyDomainAxiom axiom) {	
+		public void visit(OWLObjectPropertyDomainAxiom axiom) {
 			DGLPPropertyVisitor pty_visitor = new DGLPPropertyVisitor(0);
 			DGLPClassVisitor class_visitor = new DGLPClassVisitor(0);
-			
+
 			variable_counter = 0;
 			AtomSet body = axiom.getProperty().accept(pty_visitor);
 			AtomSet head = axiom.getDomain().accept(class_visitor);
-			
-			if(body != null && head != null)
+
+			if (body != null && head != null)
 				rules.add(RuleFactory.instance().createRule(head, body));
 		};
+
 		@Override
 		public void visit(OWLObjectPropertyRangeAxiom axiom) {
 			DGLPPropertyVisitor pty_visitor = new DGLPPropertyVisitor(0);
-						
+
 			variable_counter = 0;
 			AtomSet body = axiom.getProperty().accept(pty_visitor);
-			
+
 			DGLPClassVisitor class_visitor = new DGLPClassVisitor(variable_counter);
-			
+
 			AtomSet head = axiom.getRange().accept(class_visitor);
-			
-			if(body != null && head != null)
+
+			if (body != null && head != null)
 				rules.add(RuleFactory.instance().createRule(head, body));
 		}
 	}
-	
+
 	private class DGLPClassVisitor implements OWLClassExpressionVisitorEx<AtomSet> {
 		/* the main variable for the class */
 		private int base = 0;
-		
+
 		public DGLPClassVisitor(int cur_base) {
 			this.base = cur_base;
 		}
-		
+
 		@Override
 		public AtomSet visit(OWLClass ce) {
-			if(ce.isOWLThing()) return null;
-			
+			if (ce.isOWLThing())
+				return null;
+
 			String iri = ce.toStringID();
 			Predicate p = PredicateFactory.instance().createPredicate(iri, 1);
-			
+
 			Variable v = TermFactory.instance().getVariable(this.base);
 			List<Term> terms = new ArrayList<>();
 			terms.add(v);
 			Atom a = AtomFactory.instance().createAtom(p, terms);
-			
+
 			return new AtomSet(a);
 		};
-		
+
 		@Override
 		public AtomSet visit(OWLObjectSomeValuesFrom svf) {
 			OWLObjectPropertyExpression property = svf.getProperty();
 			OWLClassExpression filler = svf.getFiller();
-			
-			DGLPPropertyVisitor visitor = new DGLPPropertyVisitor(this.base);			
-			AtomSet p = property.accept(visitor);	
-			
+
+			DGLPPropertyVisitor visitor = new DGLPPropertyVisitor(this.base);
+			AtomSet p = property.accept(visitor);
+
 			DGLPClassVisitor nested_visitor = new DGLPClassVisitor(variable_counter);
 			AtomSet fp = filler.accept(nested_visitor);
-			
-			if(fp == null) return null;
-			
+
+			if (fp == null)
+				return null;
+
 			p.addAll(fp);
-			
-			return p;			
+
+			return p;
 		}
-		
+
 		@Override
 		public AtomSet visit(OWLObjectIntersectionOf ce) {
 			List<OWLClassExpression> expressions = ce.getOperandsAsList();
 			AtomSet atomset = new AtomSet();
-			
-			for(OWLClassExpression exp : expressions) {
+
+			for (OWLClassExpression exp : expressions) {
 				AtomSet as = exp.accept(this);
-				if(as != null)
+				if (as != null)
 					atomset.addAll(as);
-				else return null;
+				else
+					return null;
 			}
-			
+
 			return atomset;
 		}
-		
+
 		@Override
 		public AtomSet visit(OWLObjectHasSelf ce) {
-			OWLObjectPropertyExpression exp = ce.getProperty();			
+			OWLObjectPropertyExpression exp = ce.getProperty();
 			String iri = exp.getNamedProperty().toStringID();
-			
+
 			Predicate p = PredicateFactory.instance().createPredicate(iri, 2);
 			Variable v = TermFactory.instance().getVariable(this.base);
 			List<Term> terms = new ArrayList<>();
 			terms.add(v);
 			terms.add(v);
-			
+
 			Atom a = AtomFactory.instance().createAtom(p, terms);
 
 			return new AtomSet(a);
 		}
 	}
-	
+
 	private class DGLPPropertyVisitor implements OWLPropertyExpressionVisitorEx<AtomSet> {
 		private int base;
-		
+
 		public DGLPPropertyVisitor(int cur_base) {
 			this.base = cur_base;
 		}
+
 		@Override
 		public AtomSet visit(OWLObjectInverseOf property) {
-			OWLObjectPropertyExpression exp = property.getNamedProperty();			
+			OWLObjectPropertyExpression exp = property.getNamedProperty();
 			String iri = exp.getNamedProperty().toStringID();
-			
+
 			Predicate p = PredicateFactory.instance().createPredicate(iri, 2);
 			Variable v = TermFactory.instance().getVariable(this.base);
 			Variable v_ = TermFactory.instance().getVariable(++variable_counter);
-			
+
 			List<Term> terms = new ArrayList<>();
 			terms.add(v_);
 			terms.add(v);
-			
+
 			Atom a = AtomFactory.instance().createAtom(p, terms);
 
 			return new AtomSet(a);
 		}
-		
+
 		@Override
 		public AtomSet visit(OWLObjectProperty property) {
-			OWLObjectPropertyExpression exp = property.getNamedProperty();			
+			OWLObjectPropertyExpression exp = property.getNamedProperty();
 			String iri = exp.getNamedProperty().toStringID();
-			
+
 			Predicate p = PredicateFactory.instance().createPredicate(iri, 2);
 			Variable v = TermFactory.instance().getVariable(this.base);
 			Variable v_ = TermFactory.instance().getVariable(++variable_counter);
-			
+
 			List<Term> terms = new ArrayList<>();
 			terms.add(v);
 			terms.add(v_);
-			
+
 			Atom a = AtomFactory.instance().createAtom(p, terms);
 
 			return new AtomSet(a);
